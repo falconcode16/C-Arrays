6045e0119daf2e0e2982179c878e51ad14dd95fb
Rohit Patil <71687498+falconcode16@users.noreply.github.com>
2021-03-25 00:55:02 +0530

    Add files via upload

---
+++ array3.c
@@ -0,0 +1,37 @@
+
+#include<stdio.h>
+#include<math.h>
+main()
+{
+  int marks[20],i,n,sum=0,min;
+  float avg;
+  printf("enter the number of students\n");
+  scanf("%d",&n);
+  if(n<=20)
+ {
+
+printf("enter %d marks \n",n);
+ for(i=0;i<n;i++)
+
+
+  {
+      printf("enter %d marks \n",i+1);
+      scanf("%d",&marks[i]);
+  } printf("the %d marks are\n",n);
+  for(i=0;i<n;i++)
+  {printf("%d\t",marks[i]);
+
+  }
+  max=marks[20];
+for(i=0;i<r;i++)
+{
+
+    if(i<j && a[i][j]>max)
+        {
+           max=a[i][j]
+        }
+    }
+}
+print("max=%d\n",max);
+}
+}
